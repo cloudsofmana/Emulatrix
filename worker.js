6098a9227714915ea69a5472c391f42233c1445e
Leonardo Javier Russo <info@lrusso.com>
2026-04-12 17:03:38 -0300

    Update builds

--- worker.js
+++ worker.js
@@ -1,40 +1,22 @@
 const filesToCache = [
   "/",
-  "Emulatrix.css",
   "Emulatrix.htm",
   "Emulatrix.json",
   "EmulatrixFavIcon_192x192.png",
   "EmulatrixFavIcon_512x512.png",
   "EmulatrixFavIcon.svg",
   "EmulatrixShare.png",
-  "Emulatrix_DOSBox.htm",
-  "Emulatrix_DOSBox.js",
-  "Emulatrix_DOSBox.wasm",
-  "Emulatrix_GameBoy.htm",
+  "Emulatrix_DOS.js",
   "Emulatrix_GameBoy.js",
-  "Emulatrix_GameBoy.wasm",
-  "Emulatrix_GameBoyAdvance.htm",
   "Emulatrix_GameBoyAdvance.js",
-  "Emulatrix_GameBoyAdvance.wasm",
-  "Emulatrix_MAME32.htm",
-  "Emulatrix_MAME32.js",
-  "Emulatrix_MAME32.wasm",
-  "Emulatrix_MAME2003.htm",
-  "Emulatrix_MAME2003.js",
-  "Emulatrix_MAME2003.wasm",
-  "Emulatrix_Nintendo.htm",
+  "Emulatrix_Genesis.js",
+  "Emulatrix_MAME.js",
   "Emulatrix_Nintendo.js",
-  "Emulatrix_Nintendo.wasm",
-  "Emulatrix_SegaGenesis.htm",
-  "Emulatrix_SegaGenesis.js",
-  "Emulatrix_SegaGenesis.wasm",
-  "Emulatrix_SuperNintendo.htm",
   "Emulatrix_SuperNintendo.js",
-  "Emulatrix_SuperNintendo.wasm",
   "index.html",
 ]
 
-const staticCacheName = "emulatrix-v1"
+const staticCacheName = "emulatrix-v2"
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
